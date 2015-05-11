94804127c567bb6f17a2729eea45ef5ce5fafd49
komarovb <fisika80@gmail.com>
2015-05-11 18:50:47 +0200

    Initial commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Book.destroy_all
+
+Book.create! ([{
+	title: "Hyperion",
+	author: "Dan Simmons",
+	description: "Description 1",
+	amazon_id: "0553283685",
+	rating: 5,
+	finished_on: 10.days.ago
+},
+{
+	title: "Mindstorms",
+	author: "Seymour A. Papert",
+	description: "Description 2",
+	amazon_id: "0465046746",
+	rating: 3,
+	finished_on: nil
+}
+	])
