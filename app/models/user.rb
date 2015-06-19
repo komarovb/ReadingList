c414d2a16e65cfb9945987d0fbfb32d1b762b4e2
komarovb <fisika80@gmail.com>
2015-06-19 22:23:18 +0200

    Basic back end validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ActiveRecord::Base
 	has_secure_password
 	has_many :user_books
 	has_many :books, through: :user_books
+	validates :first_name, :last_name, presence: true
+	validates_uniqueness_of :email
+	validates :password, length: { minimum: 6, maximum: 24, message: "Password should be from 6 up to 24 characters!"}
+
 end
